bded981f5ca94225cc99d02f3a0a79c0e7db91e4
iandrei250 <aiordache020@gmail.com>
2025-02-10 23:24:06 +0100

    update: draft

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,13 +4,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { hkcouple } from './assets/index.ts';
+import { BottomNavigation, BottomNavigationAction, Container } from '@mui/material';
+import { mainText } from '../src/constants/constants.ts';
 
 
 
 const App = () => {
   return (
     <Box fontFamily={'Roboto'} height={"100dvh"} bgcolor={"#FFC5D9"}>
-      
+      <Box>
+        <img src={hkcouple}/>
+      </Box>
+      <Container maxWidth={"sm"}>
+        <Box>{mainText}</Box>
+      </Container>
     </Box>
   );
 }
---
+++ my-app/src/assets/index.ts
@@ -0,0 +1,3 @@
+import hkcouple from "./hello-kitty.png";
+
+export {hkcouple};
